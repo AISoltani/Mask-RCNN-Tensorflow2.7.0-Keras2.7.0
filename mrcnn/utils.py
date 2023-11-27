771a162653828073bd469a41ba65648205816256
AIsoltani <amirsoltani_os@hotmail.com>
2023-11-27 09:51:53 +0330

    Update utils.py

--- mrcnn/utils.py
+++ mrcnn/utils.py
@@ -27,9 +27,9 @@ from distutils.version import LooseVersion
 COCO_MODEL_URL = "https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5"
 
 
-############################################################
+###########################################################
 #  Bounding Boxes
-############################################################
+###########################################################
 
 def extract_bboxes(mask):
     """Compute bounding boxes from masks.
